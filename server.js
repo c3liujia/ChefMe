6d1afb569a80d5b78b136fa1e4c74f8825e5fd43
Henry Liu <jiah.liu@mail.utoronto.ca>
2017-10-24 17:20:31 -0400

    Set up MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 app.use(express.static('src'));
 
 var PORT = process.env.PORT || 8080;
@@ -13,3 +14,29 @@ app.use(function (req, res, next){
     console.log("HTTP request", req.method, req.url, req.body);
     return next();
 });
+
+var url = 'mongodb://localhost:27017/test';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
+
+
+//***Below used to test mongo***
+// var insertDocument = function(db, callback) {
+//    db.collection('test').insertOne( {
+//    	"test" : "Hello world"
+//    }, function(err, result) {
+//     assert.equal(err, null);
+//     console.log("Inserted a document into the restaurants collection.");
+//     callback();
+//   });
+// };
+
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   insertDocument(db, function() {
+//       db.close();
+//   });
+// });
